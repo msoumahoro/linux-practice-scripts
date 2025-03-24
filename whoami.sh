55c6dfe2b72e7c0455675a6e59f032f5497d7c60
msoumahoro <aisha.kenneh@gmail.com>
2025-03-24 17:30:30 -0500

    Our first whoami and hello world files

---
+++ whoami.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This file containes information about the current user
+
+echo " Hello! Welcome to this system summary."
+
+echo " Name: Massa "
+echo " From: C√¥te d'Ivoire (Ivory Coast)"
+echo " Goal: Future DevOps Engineer"
+echo " System User: $(whoami)"
+echo "Ì≥Ö Date: $(date '+%A, %B %d, %Y')"
+echo "‚è∞ Time: $(date '+%I:%M %p')"
+
+name="MASSA"
+echo " Hi my name is $name"
+
